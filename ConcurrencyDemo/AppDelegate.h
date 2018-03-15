7959a067ba9db5e69f14d2da12a586b90bebd534
张诗健 <zsjcwy_xcode@163.com>
2018-03-15 16:05:39 +0800

    first commit

---
+++ ConcurrencyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConcurrencyDemo
+//
+//  Created by 讯心科技 on 2018/3/15.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConcurrencyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConcurrencyDemo
+//
+//  Created by 讯心科技 on 2018/3/15.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
