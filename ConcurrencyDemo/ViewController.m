8240c7e8bd3c7a242084207835d81539b624f4ff
张诗健 <zsjcwy_xcode@163.com>
2018-05-29 17:39:33 +0800

    commit

--- ConcurrencyDemo/ViewController.m
+++ ConcurrencyDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "CustomOperation.h"
+
 
 @interface ViewController ()
 
@@ -21,32 +23,54 @@
     // 创建一个操作队列对象
     NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
     // 设置操作队列的最大并发数
-    operationQueue.maxConcurrentOperationCount = 1;
+    operationQueue.maxConcurrentOperationCount = 5;
     
     // 创建一个NSBlockOperation
     NSBlockOperation *blockOperation = [NSBlockOperation blockOperationWithBlock:^{
+        
+        for (int i = 0; i < 20; i++)
+        {
+            
+        }
         
         NSLog(@"操作 ------> 1");
     }];
     
     NSInvocationOperation *invocationOperation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(invocationOperation) object:nil];
+    
+    
+    CustomOperation *customOperation = [[CustomOperation alloc] initWithIdentifier:@"4"];
 
     // 在将操作添加到操作队列之前，配置操作依赖性，invocationOperation会等到blockOperation完成后才开始执行
-    //[invocationOperation addDependency:blockOperation];
+    [invocationOperation addDependency:blockOperation];
+    
+    [blockOperation addDependency:customOperation];
 
     [operationQueue addOperation:blockOperation];
 
     [operationQueue addOperation:invocationOperation];
+    
+    [operationQueue addOperation:customOperation];
 
     // 直接添加一个操作到operationQueue中
     [operationQueue addOperationWithBlock:^{
 
+        for (int i = 0; i < 20; i++)
+        {
+            
+        }
+        
         NSLog(@"操作 ------> 3");
     }];
 }
 
 - (void)invocationOperation
 {
+    for (int i = 0; i < 20; i++)
+    {
+        
+    }
+    
     NSLog(@"操作 ------> 2");
 }
 
