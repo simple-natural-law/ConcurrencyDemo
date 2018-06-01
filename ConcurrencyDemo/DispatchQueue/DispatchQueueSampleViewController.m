034e0a68d855ea1a573cbb7bddfd0947bc844c91
zhangshijian <zsjcwy_xcode@163.com>
2018-06-01 09:54:32 +0800

    commit

--- ConcurrencyDemo/DispatchQueue/DispatchQueueSampleViewController.m
+++ ConcurrencyDemo/DispatchQueue/DispatchQueueSampleViewController.m
@@ -27,10 +27,10 @@
 {
     [super viewDidAppear:animated];
     
-    // 创建一个串行调度队列，添加到串行调度队列的任务按照先进先出的顺序串行执行。(创建队列时，为队列指定一个名称以便调试)
+    // 创建一个串行调度队列，添加到串行调度队列的任务按照先进先出的顺序被串行调度（需要等待上一个任务执行完毕后才会继续调度下一个任务）到合适的线程上执行。(创建队列时，为队列指定一个名称以便调试)
     dispatch_queue_t serialQueue = dispatch_queue_create("com.jian.serialQueue", DISPATCH_QUEUE_SERIAL);
     
-    // 将任务1添加到serialQueue中，该任务相对于调用dispatch_async函数的线程异步执行。
+    // 将任务1添加到serialQueue中，任务1会被serialQueue调度到一个新线程（并非调用dispatch_async函数的线程）上执行。dispatch_async函数是一个异步函数，其在任务1被调度到新线程之后就直接返回了。
     dispatch_async(serialQueue, ^{
 
         for (int i = 0; i < 100; i++)
@@ -38,9 +38,10 @@
             
         }
         NSLog(@"任务 ----> 1");
+        NSLog(@"1 ----> %@",[NSThread currentThread]);
     });
     
-    // 将任务2添加到serialQueue中，该任务相对于调用dispatch_sync函数的线程同步执行。
+    // 将任务2添加到serialQueue中，任务2会被serialQueue调度到调用dispatch_sync函数的线程上执行。dispatch_sync函数是一个同步函数，其会在任务2被调度到调用dispatch_sync函数的线程上并且执行完毕后才会返回。
     dispatch_sync(serialQueue, ^{
 
         for (int i = 0; i < 20; i++)
@@ -48,12 +49,13 @@
             
         }
         NSLog(@"任务 ----> 2");
+        NSLog(@"2 ----> %@",[NSThread currentThread]);
     });
     
-    // 创建一个并行调度队列，添加到并行调度队列中的任务按照先进先出的顺序并行执行。(创建队列时，为队列指定一个名称以便调试)
+    // 创建一个并行调度队列，添加到并行调度队列的任务按照先进先出的顺序被并行调度（不用等待上一个任务执行完毕，就继续调度下一个任务）到合适的线程上执行。(创建队列时，为队列指定一个名称以便调试)
     dispatch_queue_t concurrentQueue = dispatch_queue_create("com.jian.concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
     
-    // 将任务3添加到concurrentQueue中，该任务相对于调用dispatch_sync函数的线程同步执行。
+    
     dispatch_sync(concurrentQueue, ^{
         
         for (int i = 0; i < 20; i++)
@@ -63,7 +65,7 @@
         NSLog(@"任务 ----> 3");
     });
     
-    // 将任务4添加到concurrentQueue中，该任务相对于调用dispatch_async函数的线程异步执行。
+    
     dispatch_async(concurrentQueue, ^{
         
         for (int i = 0; i < 100; i++)
